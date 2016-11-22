b0b576b250b6a37f9fe3fe31555f04dd71be1d9e
huangshiwen <1026817600@.com>
2016-11-22 22:27:13 +0800

    调整网络请求框架，添加第五个界面子

---
+++ app/src/main/java/com/honliv/honlivmall/model/activity/PaymentCenterModel.java
@@ -0,0 +1,55 @@
+package com.honliv.honlivmall.model.activity;
+
+import com.honliv.honlivmall.GloableParams;
+import com.honliv.honlivmall.bean.AddressInfo;
+import com.honliv.honlivmall.bean.DefaultParas;
+import com.honliv.honlivmall.bean.GifInfo;
+import com.honliv.honlivmall.bean.PayShip;
+import com.honliv.honlivmall.contract.ActivityContract;
+import com.honliv.honlivmall.engine.AddressPostUtils;
+import com.honliv.honlivmall.engine.CategoryPostUtils;
+import com.honliv.honlivmall.engine.HomePostUtils;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * Created by Rodin on 2016/11/21.
+ */
+public class PaymentCenterModel implements ActivityContract.PaymentCenterModel {
+    @Override
+    public Observable<Float> getServiceTotalPrice(int proSaleId, int groupBuyId, List<JSONObject> productList) {
+        Observable<Float> result = CategoryPostUtils.GetTotalPrice(GloableParams.USERID, proSaleId, groupBuyId,
+                productList);
+        return result;
+    }
+
+    @Override
+    public Observable<Float> getServiceFreight(int shipId, int shipTypeId, List<JSONObject> productJsobList, float allCouponMoney) {
+        Observable<Float> result = CategoryPostUtils.GetFreight(GloableParams.USERID, shipId, shipTypeId,
+                productJsobList, allCouponMoney);
+        return result;
+    }
+
+    @Override
+    public Observable<GifInfo> getServiceGifInfo(List<JSONObject> productList, float couponPrice) {
+        Observable<GifInfo> result = CategoryPostUtils.GetGiftInfo(GloableParams.USERID, productList, couponPrice);
+        return result;
+    }
+
+    @Override
+    public Observable<List<AddressInfo>> getServiceAddressList(int pageIndex, int pageNum) {
+        Observable<List<AddressInfo>> result = AddressPostUtils.addresslist(GloableParams.USERID,  pageIndex,  pageNum);
+        return result;
+    }
+
+    @Override
+    public Observable<List<PayShip>> getServicePayList() {
+        Observable<List<PayShip>> result = AddressPostUtils.GetPayList();
+        return result;
+    }
+}
