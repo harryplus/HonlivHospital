dfc1d45048def14e7aaaa93fee684a29de248dd2
huangshiwen <1026817600@.com>
2016-11-22 22:27:00 +0800

    调整网络请求框架，添加第五个界面子

---
+++ app/src/main/java/com/honliv/honlivmall/contract/ActivityContract.java
@@ -0,0 +1,70 @@
+package com.honliv.honlivmall.contract;
+
+import com.honliv.honlivmall.activity.CoreBaseView;
+import com.honliv.honlivmall.bean.AddressInfo;
+import com.honliv.honlivmall.bean.DefaultParas;
+import com.honliv.honlivmall.bean.GifInfo;
+import com.honliv.honlivmall.bean.PayShip;
+import com.honliv.honlivmall.model.CoreBaseModel;
+import com.honliv.honlivmall.presenter.CoreBasePresenter;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by Rodin on 2016/11/21.
+ */
+public interface ActivityContract {
+    interface FlashModel extends CoreBaseModel {
+
+        Observable<DefaultParas> getServiceDafault();
+    }
+
+    interface PaymentCenterModel extends CoreBaseModel {
+        Observable<Float> getServiceTotalPrice(int proSaleId, int groupBuyId, List<JSONObject> productList);
+
+        Observable<Float> getServiceFreight(int id, int shipId, List<JSONObject> productJsobList, float i);
+
+        Observable<GifInfo> getServiceGifInfo(List<JSONObject> productList, float couponPrice);
+
+        Observable<List<AddressInfo>> getServiceAddressList(int arg, int arg2);
+
+        Observable<List<PayShip>> getServicePayList();
+    }
+
+    interface FlashView extends CoreBaseView {
+
+        void updateView(DefaultParas result);
+    }
+
+    interface PaymentCenterView extends CoreBaseView {
+        void updateServiceTotalPrice(Float result);
+
+        void updateServiceFreight(Float result);
+
+        void updateServiceGifInfo(GifInfo result);
+
+        void updateServiceAddressList(List<AddressInfo> result);
+
+        void updateServicePayList(List<PayShip> result);
+    }
+
+    abstract class PaymentCenterPresenter extends CoreBasePresenter<PaymentCenterModel, PaymentCenterView> {
+        public abstract void getServiceTotalPrice(int proSaleId, int groupBuyId, List<JSONObject> productList);
+
+        public abstract void getServiceFreight(int id, int shipId, List<JSONObject> productJsobList, int i);
+
+        public abstract void getServiceGifInfo(List<JSONObject> productList, float couponPrice);
+
+        public abstract void getServiceAddressList(int arg, int arg2);
+
+        public abstract void getServicePayList();
+    }
+
+    abstract class FlashPresenter extends CoreBasePresenter<FlashModel, FlashView> {
+        public abstract void getServiceDafault();
+    }
+}
